ecd9a4704caea8834bcb0921a9b4ef0d0a4f15d4
Yusuke Matsushita <yskmjp@yskm-mbp.local>
2017-10-18 19:26:53 +0900

    Initial Commit

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>\rspd=110\\vct=145\お待たせしましたーっ\pau=400\ご注文のお品です</source>
+            <comment>Text</comment>
+            <translation type="unfinished">\rspd=110\\vct=145\お待たせしましたーっ\pau=400\ご注文のお品です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=145\お空きのグラスを、\rspd=106\\vct=152\おさげします！！</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=145\お空きのグラスを、\rspd=106\\vct=152\おさげします！！</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>\rspd=110\\vct=145\お空きのグラスを、\rspd=106\\vct=152\おさげします！！\rspd=110\\vct=145\お手数ですが、ボクのおぼ|ンに乗せていただけますか？</source>
+            <comment>Text</comment>
+            <translation type="unfinished">\rspd=110\\vct=145\お空きのグラスを、\rspd=106\\vct=152\おさげします！！\rspd=110\\vct=145\お手数ですが、ボクのおぼ|ンに乗せていただけますか？</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>\rspd=110\\vct=137\ありがとう｜\vct=144\ございましたーッ、、\pau=500</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=137\ありがとう｜\vct=144\ございましたーッ、、\pau=500</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>\rspd=110\\vct=137\ありがとう｜\vct=144\ございましたーッ、、</source>
+            <comment>Text</comment>
+            <translation type="unfinished">\rspd=110\\vct=137\ありがとう｜\vct=144\ございましたーッ、、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>\rspd=110\\vct=145\お空きのグラスを、\rspd=106\\vct=152\おさげします！！\rspd=110\\vct=145\お手数ですが、ボクのおぼ|ンに乗せていただけますか？</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=145\お空きのグラスを、\rspd=106\\vct=152\おさげします！！\rspd=110\\vct=145\お手数ですが、ボクのおぼ|ンに乗せていただけますか？</translation>
+        </message>
+    </context>
+</TS>
